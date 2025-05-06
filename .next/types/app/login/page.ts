88447709987378cedfbf3721867a7528856e8acf
banhmisocute <gbaoo.1102@gmail.com>
2025-05-06 09:52:22 +0700

    no thing

--- .next/types/app/login/page.ts
+++ .next/types/app/login/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Documents\Midary-SEPM\Mindary-Frontend-FTECH\app\login\page.tsx
+// File: C:\Users\ADMIN\Documents\project\mindary_v2\Mindary-Frontend-FTECH\app\login\page.tsx
 import * as entry from '../../../../app/login/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
